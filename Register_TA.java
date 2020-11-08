1d3a3c687a64ad15fdd5910b9678d0df4cdfe4fb
Sim Waraich <48163823+SimWaraich@users.noreply.github.com>
2020-11-08 13:20:13 -0800

    Update Register_TA.java
    changed to student access by default for security purposes 115

--- Register_TA.java
+++ Register_TA.java
@@ -112,7 +112,7 @@ public class Register_TA extends AppCompatActivity {
                         user.put("phone",phone);
                         
                         //check if our user is a TA or student:
-                        user.put("TA?","1")
+                        user.put("Student?","1")
                         
                         documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
